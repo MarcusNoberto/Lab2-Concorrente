a7f8edd2145c42a0768ce6c93fc597f355a1ade4
marcusnoberto <marcus.ideao@ccc.ufcg.edu.br>
2023-09-23 11:18:17 -0300

    lab3

--- java/run.sh
+++ java/run.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
 
-ROOT_DIRECTORY=../dataset/
+# Verifica se o Gradle está instalado
+if ! command -v gradle &> /dev/null; then
+    echo "Gradle não encontrado. Certifique-se de que o Gradle está instalado e configurado corretamente."
+    exit 1
+fi
 
-RESULT=../tmp/result
-mkdir -p $RESULT
-
-count_word_dir() {
-	directory="$1"
-	resultado=$(gradle -q runApp --args="$directory")
-	archive_name=$(echo "$directory" | cut -c 12-19)
-	echo "$resultado"> "$RESULT/$archive_name.txt"
-}
-
-for pasta in $(find "$ROOT_DIRECTORY" -type d -links 2); do
-	count_word_dir "$pasta" &
-done
-
-wait
-
-total_palavras=0
-for resultado_file in "$RESULT"/*.txt; do
-        resultado=$(cat "$resultado_file")  # Remova as aspas duplas ao redor de $resultado_file
-        total_palavras=$((total_palavras + resultado))
-done
-echo "$total_palavras"
-
-rm -rf "../tmp"
+# Executa o comando 'gradle runApp'
+gradle runApp
